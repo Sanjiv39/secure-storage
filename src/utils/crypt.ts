081de71855ce7355d0954da39bccbe1a6c562ecd
Sanjiv39 <sanjiv.samal39@gmail.com>
2025-05-11 21:42:22 +0530

    changes : added crypto

--- src/utils/crypt.ts
+++ src/utils/crypt.ts
@@ -1,1 +1,30 @@
-export type 
+import CryptoJS from "crypto-js";
+
+export const secret =
+  process?.env?.SECURE_STORAGE_SECRET || "x1bQYQA4vSEcR6RQ05XtJg";
+
+export const encrypt = (value: string) => {
+  try {
+    if (typeof value !== "string") {
+      throw new Error("Value must be string");
+    }
+    const str = CryptoJS.AES.encrypt(value, secret).toString();
+    return str;
+  } catch (err) {
+    console.error("Error secure storage => encrypt :", err);
+    return null;
+  }
+};
+
+export const decrypt = (value: string) => {
+  try {
+    if (typeof value !== "string") {
+      throw new Error("Value must be string");
+    }
+    const str = CryptoJS.AES.decrypt(value, secret).toString();
+    return str;
+  } catch (err) {
+    console.error("Error secure storage => decrypt :", err);
+    return null;
+  }
+};
