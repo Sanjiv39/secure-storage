e2bfcbacde2ec65ddd07aa59fd35d4a760837d74
Sanjiv39 <sanjiv.samal39@gmail.com>
2025-05-12 10:37:37 +0530

    build : es6 module conversion to mjs

--- src/test/store.ts
+++ src/test/store.ts
@@ -2,6 +2,12 @@ import { encrypt, decrypt } from "../utils/crypt";
 import { modifyDataToString, getDataFromModifiedString } from "../utils/store";
 
 const data = { message: "test" };
-const mod = modifyDataToString(data) as string
+const mod = modifyDataToString(data) as string;
 const enc = encrypt(mod);
-console.log(mod, enc, decrypt(enc as string));
+const dec = decrypt(enc as string);
+const decoded = getDataFromModifiedString(dec as string);
+console.log("Testing store encryption and decryption.......................................")
+console.log("Data :", mod);
+console.log("Encrypted :", enc);
+console.log("Decrypted :", dec);
+console.log("Decoded data :", decoded?.data);
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
+  // entry: ["src/**/*.[jt]s"], // for multiple files
   entry: ["src/index.ts"],
   format: ["cjs", "esm"],
   dts: true,
   outDir: "dist",
+  splitting: true,
   clean: true,
+  // sourcemap: true,
+  minify: true,
+  outExtension({ format }) {
+    return format === "esm" ? { js: ".mjs" } : { js: ".cjs" };
+  },
   env: {
     SECURE_STORAGE_SECRET: "x1bQYQA4vSEcR6RQ05XtJg",
     SECURE_STORAGE_PREFIX: "@secst",
