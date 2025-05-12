837e4cacedcaa596d0d9f9adb3fbc5b9634b8b7c
Sanjiv39 <sanjiv.samal39@gmail.com>
2025-05-12 23:13:26 +0530

    config : tsup clean build dir

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   // entry: ["src/**/*.[jt]s"], // for multiple files
-  entry: ["src/index.ts"],
+  entry: ["src/index.ts", "!src/(utils|test).(ts|js)"],
   format: ["cjs", "esm"],
   platform: "browser",
   target: ["node16"],
@@ -12,6 +12,7 @@ export default defineConfig({
   clean: true,
   sourcemap: true,
   minify: true,
+  // bundle: false,
   env: {
     SECURE_STORAGE_SECRET: "x1bQYQA4vSEcR6RQ05XtJg",
     SECURE_STORAGE_PREFIX: "@secst",
